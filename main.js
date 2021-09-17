2ee71cc66143b825694b65d22a59a81f5276d4f8
CoWiMa <84484068+CoWiMa@users.noreply.github.com>
2021-09-16 20:58:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("My very first console VS Code project!");
+var first_name = "Max";
+var last_name = "\"ssertneF\" backwards";
+console.log(first_name + last_name);
+var a = 15;
+var b = 12;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
