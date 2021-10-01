cda98f2759b03599306f01e27e68e9900c65826a
CoWiMa <84484068+CoWiMa@users.noreply.github.com>
2021-09-30 20:58:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-var concept=["prime numbers", "composite numbers", "0 and 1", "circles", "fractals" ];
-var images=["https://sciencenotes.org/wp-content/uploads/2021/06/Prime-Numbers-to-100.png", "https://cdn1.byjus.com/wp-content/uploads/2020/07/Composite-Numbers.png", "https://lh3.googleusercontent.com/proxy/v4fuGfvt2whDZ6pMgp7mAD1rOzNAhsn7MOCyFKNN7Zkvvx0veXm4KpnMGf9z8d1Qx3nQSt3gIb4acIdb0qsrGzBZRNSRWCkfMde0TZcNU30IkVbr0TWoArmLNCijm1KpRgyDqtUUjdLoMQLkLgTgXgeJAQs", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT56CnuNMqSWDMsNweJVgfZffJJ5p_bY5XsuqXMMN22JnGhpRCjaVVnkYCv1QCLpIYeGHY&usqp=CAU", "https://users.math.yale.edu/public_html/People/frame/Fractals/Vlinders.gif"];
-var index=0;
-function next_topic()
-{
-   index=index+1;
-   var length_of_array=concept.length;
-if (index == length_of_array)
-{
-    index=0;
+menu_list_array = ["Veg Margherita Pizza","Cauliflower Thin Crust Pizza", "Neapolitan Pizza", "St. Louis Pizza"];
 
+function getmenu(){
+var htmldata = "";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
 }
-document.getElementById("advanced_math_image").src=images[index];
-document.getElementById("advanced_math_learning").innerHTML=concept[index];
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+var imgtags='<img id="im1" src="https://snehalbsawant.github.io/ADV-C79/images/pizza.png"/>'
+menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_topping(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
