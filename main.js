35286d60df84d040029fb4c9323c9b2ab92c844a
CoWiMa <84484068+CoWiMa@users.noreply.github.com>
2021-10-13 20:40:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
-menu_list_array = ["Veg Margherita Pizza","Cauliflower Thin Crust Pizza", "Neapolitan Pizza", "St. Louis Pizza"];
-
-function getmenu(){
-var htmldata = "";
-menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-        }
-        document.getElementById("display_menu").innerHTML = htmldata;
-        //give the appropriate id name as display_menu  
+var names_of_people_array = [];
+function submit()
+{
+	var guest_name = document.getElementById("name1").value;
+	names_of_people_array.push(guest_name);
+	document.getElementById("display_name").innerHTML = names_of_people_array.toString();
 }
-
-function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-var imgtags='<img id="im1" src="https://snehalbsawant.github.io/ADV-C79/images/pizza.png"/>'
-menu_list_array.sort();
-        htmldata=""
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
-        }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;		
-      }
-
-function add_topping(){
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-	add_item();
+function searching()
+{
+	var name = document.getElementById("srch").value;
+	for (let index = 0; index < names_of_people_array.length; index++) {
+		if(name== array[index])
+		{
+			document.getElementById("p2").innerHTML = "found";
+		}	
+	}
 }
